330d737b7c8d523f6aaf89c7c53cecfcd9f5d454
JoshuaW13 <josh.walters@shaw.ca>
2022-11-19 19:54:12 -0700

    added wolf

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
 //Varaibles
-let animalImages = ["images/capybara.gif","images/panda.gif","images/frog.gif"];
+let animalImages = ["images/capybara.gif","images/panda.gif","images/frog.gif","images/wolf.gif"];
 
 function set_canvas(){
     let canvas = document.querySelector('.canvas');
