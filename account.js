a626e326388fcf2484ab649bfe913bfa186c9b54
Kaito Sugimura <sugimurakaito@gmail.com>
2022-11-20 11:05:58 -0700

    a

--- account.js
+++ account.js
@@ -4,7 +4,14 @@ const NAME = document.getElementById("NAME");
 const EMAIL = document.getElementById("EMAIL");
 const HISTORY = document.getElementById("HISTORY");
 
-NAME.innerHTML = getUserData(getCurrentUser());
-// HISTORY.innerHTML = getCharitiesDonatedTo(auth.currentUser);
+const userData = await getUserData(getCurrentUser());
+NAME.innerHTML = userData.name;
+EMAIL.innerHTML = userData.email;
+
+const userDonations = await getCharitiesDonatedTo(getCurrentUser());
+HISTORY.innerHTML = userDonations.map(
+  (data) => `
+    <li>$<span class="donation-amount">${data.total}</span> - <span class="donation-name">${data.history}</span></li>`
+).join("");
 
 // console.log(async getCharitiesDonatedTo(auth.currentUser));
