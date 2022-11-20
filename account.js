e0e3bcd27697b16f63e72b54cbfb5900669d0a64
Kaito Sugimura <sugimurakaito@gmail.com>
2022-11-20 09:43:21 -0700

    account

--- account.js
+++ account.js
@@ -1,3 +1,5 @@
+import { getUserData } from "./app.js";
+
 const NAME = document.getElementById("NAME");
 const EMAIL = document.getElementById("EMAIL");
 const HISTORY = document.getElementById("HISTORY");
