62ad774996483d2127e6e2a2a006dbdad750d293
Danny Duong <dannyduong4.00@outlook.com>
2022-11-20 11:00:04 -0700

    Fix account page

--- account.js
+++ account.js
@@ -1,10 +1,28 @@
 import { getCurrentUser, getUserData, getCharitiesDonatedTo } from "./app.js";
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+const auth = getAuth();
 
 const NAME = document.getElementById("NAME");
 const EMAIL = document.getElementById("EMAIL");
-const HISTORY = document.getElementById("HISTORY");
+const HISTORY = document.querySelector(".donate");
 
-NAME.innerHTML = getUserData(getCurrentUser());
+auth.onAuthStateChanged(async user => {
+  if (user) {
+    const userData = await getUserData(user);
+    NAME.innerHTML = userData.name;
+    EMAIL.innerHTML = userData.email;
+
+    HISTORY.innerHTML = userData.donationHistory.map(donation => `
+      <li>
+        $<span class="donation-amount">${donation.amount}</span> -
+        <span class="donation-name">${donation.name}</span>
+      </li>
+    `).join('');
+  } else {
+    NAME.innerHTML = '';
+    EMAIL.innerHTML = '';
+  }
+})
 // HISTORY.innerHTML = getCharitiesDonatedTo(auth.currentUser);
 
 // console.log(async getCharitiesDonatedTo(auth.currentUser));
